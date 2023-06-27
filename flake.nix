6e1932f7f26de2465867336bf96fdda2e4cf49f5
Ingo Ruhnke <grumbel@gmail.com>
2023-06-27 16:29:00 +0200

    Update to mpg123-1.31.3 and nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
 
-    mpg123_src.url = "https://downloads.sourceforge.net/project/mpg123/mpg123/1.30.2/mpg123-1.30.2.tar.bz2";
+    mpg123_src.url = "https://downloads.sourceforge.net/project/mpg123/mpg123/1.31.3/mpg123-1.31.3.tar.bz2";
     mpg123_src.flake = false;
   };
 
@@ -17,7 +17,7 @@
 
           mpg123 = pkgs.stdenv.mkDerivation {
             pname = "mpg123";
-            version = "1.30.2";
+            version = "1.31.3";
 
             src = mpg123_src;
           };
